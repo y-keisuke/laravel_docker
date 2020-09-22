9cdd4955d448f2157ae767aa8f3a83342e788c22
keisuke yamauchi <keisuke.yamauchi.321@gmail.com>
2020-09-22 20:16:29 +0900

    improve 改行修正

--- docker/db/sql/insert-testdata.sql
+++ docker/db/sql/insert-testdata.sql
@@ -1,3 +1,2 @@
-INSERT INTO users
-    (name, email, password)
+INSERT INTO users (name, email, password)
     VALUES ('test_name', 'example@example.com', '$2y$10$sMIPQPRk58rBOlFqF1LcKe/wdOagyU6CGdV7ZwifLeGc6001AiWra');
