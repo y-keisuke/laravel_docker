a1b327f7b035d3a339b385c2f6647842c751f8d7
keisuke yamauchi <keisuke.yamauchi.321@gmail.com>
2020-09-22 20:16:08 +0900

    update コメント追加

--- docker/db/sql/init-database.sh
+++ docker/db/sql/init-database.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# テーブルを作成する
 mysql -u docker -pdocker laravel_docker < "/docker-entrypoint-initdb.d/create-table.sql"
+# レコードを作成する
 mysql -u docker -pdocker laravel_docker < "/docker-entrypoint-initdb.d/insert-testdata.sql"
--- initialization.sh
+++ initialization.sh
@@ -4,5 +4,7 @@
 docker-compose exec app bash -c "chmod -R 777 storage/ bootstrap/cache/"
 
 # DBに初期データを作成
+## シェルスクリプトを実行するためにパーミッションを変更
 docker-compose exec db bash -c "chmod 0755 /docker-entrypoint-initdb.d/init-database.sh"
+## シェルスクリプトを実行
 docker-compose exec db bash -c "./docker-entrypoint-initdb.d/init-database.sh"
